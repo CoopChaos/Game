c9377efa6f4234b58b1c406c5f15b4ef5a71ce1c
Philipp Hinz <phisn@outlook.de>
2021-11-20 13:13:29 +0100

    fixed player

--- Assets/CoopChaos/Scripts/Client/Game/ClientPlayerCharacterBehaviour.cs
+++ Assets/CoopChaos/Scripts/Client/Game/ClientPlayerCharacterBehaviour.cs
@@ -8,28 +8,31 @@ namespace CoopChaos
     [RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput))]
     public class ClientPlayerCharacterBehaviour : NetworkBehaviour
     {
-        [SerializeField]
-        private float speed = 5f;
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private GameObject characterCamera;
 
         private Rigidbody2D rigidbody;
         private InputAction moveInputAction;
 
         public override void OnNetworkSpawn()
         {
+            Debug.Assert(characterCamera != null, "Character camera is null");
+            
             if (!IsClient || !IsOwner)
             {
                 enabled = false;
+                characterCamera.SetActive(false);
             }
 
             var playerInput = GetComponent<PlayerInput>();
             moveInputAction = playerInput.actions["move"];
-            rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody = GetComponent<Rigidbody2D>(); 
         }
 
         private void Update()
         {
             Vector2 moveInput = moveInputAction.ReadValue<Vector2>();
-            rigidbody.position += moveInput * Time.deltaTime * speed;
+            rigidbody.velocity += moveInput * Time.deltaTime * speed;
         }
     }
 }
