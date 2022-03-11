bf30c061b6929d1bfaa94e6a33094d4388f629fa
Philipp Hinz <phisn@outlook.de>
2022-03-11 18:34:18 +0100

    fix cannonroomstate

--- Assets/CoopChaos/Scripts/Shared/Game/Rooms/CannonRoomState.cs
+++ Assets/CoopChaos/Scripts/Shared/Game/Rooms/CannonRoomState.cs
@@ -1,12 +1,19 @@
 using Unity.Netcode;
+using UnityEngine;
 
 namespace CoopChaos
 {
     public class CannonRoomState : InteractableObjectStateBase
     {
+        private ServerCannonRoom server;
+        
         private NetworkVariable<bool> isBlocked;
-
-        public NetworkVariable<bool> IsBlocked => isBlocked;
+        
+        // loading in percent, 1.0f == loaded
+        public NetworkVariable<float> BulletLoad { get; private set; }
+        public NetworkVariable<float> Angle { get; private set; }
+        
+        public NetworkVariable<bool> IsBlocked { get; private set; }
 
         public override void OnNetworkSpawn()
         {
@@ -15,11 +22,30 @@ namespace CoopChaos
             isBlocked.OnValueChanged += HandleIsBlockedChanged;
         }
 
+        private int i = 0;
+        
+        [ServerRpc]
+        public void ShootServerRpc()
+        {
+            Debug.Log("Shoot called" + ++i);
+            server.Shoot();
+        }
+        
+        [ServerRpc]
+        public void SetAngleServerRpc(float angle)
+        {
+            Angle.Value = angle;
+        }
+
         protected override void Awake()
         {
             base.Awake();
             
             isBlocked = new NetworkVariable<bool>(false);
+            BulletLoad = new NetworkVariable<float>(1.0f);
+            Angle = new NetworkVariable<float>(0.0f);
+            
+            server = GetComponent<ServerCannonRoom>();
         }
         
         private void HandleIsBlockedChanged(bool blocked, bool oldBlocked)
