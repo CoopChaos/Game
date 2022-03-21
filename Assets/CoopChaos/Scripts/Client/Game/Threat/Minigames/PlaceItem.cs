d435f2b20fffaca99691340cb5bcacc9fe7ea2a5
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2022-03-21 21:47:59 +0100

    bootstrapped palceitem minigame

--- Assets/CoopChaos/Scripts/Client/Game/Threat/Minigames/PlaceItem.cs
+++ Assets/CoopChaos/Scripts/Client/Game/Threat/Minigames/PlaceItem.cs
@@ -24,14 +24,66 @@ namespace CoopChaos
 
         int correctItem = 0;
 
-        public override void StartMinigame()
+        int currentItem = 0;
+
+        int correctCount = 0;
+
+        public override async void StartMinigame()
         {
             base.StartMinigame();
+
+            itemMap = new int[grabFields.Length];
+            itemPutMap = new int[placeFields.Length];
+
+            for (int i = 0; i < grabFields.Length; i++)
+            {
+                int rnd = UnityEngine.Random.Range(0, items.Length);
+
+                itemMap[i] = rnd;
+                itemPutMap[i] = rnd;
+            }
+
+            for (int i = 0; i < grabFields.Length; i++)
+            {
+                int x = i;
+                grabFields[i].GetComponent<Image>().sprite = items[itemMap[i]];
+                grabFields[i].onClick.AddListener(() => { OnGrabFieldClick(x); });
+            }
+
+            for (int i = 0; i < placeFields.Length; i++)
+            {
+                int x = i;
+                placeFields[i].onClick.AddListener(() => { OnPlaceFieldClick(x); });
+            }
+        }
+
+        private void OnPlaceFieldClick(int x)
+        {
+            if(itemPutMap[x] == currentItem)
+            {
+                placeFields[x].GetComponent<Image>().sprite = items[itemPutMap[x]];
+                correctCount++;
+                placeFields[x].interactable = false;
+            }
+        }
+
+        private void OnGrabFieldClick(int i)
+        {
+            currentItem = itemMap[i];
+            Debug.Log("Clicked on grab field " + i);
         }
 
         void ButtonClicked()
         {
             FinishMinigame();
+        }
+
+        void Update()
+        {
+            if (correctCount == grabFields.Length)
+            {
+                FinishMinigame();
+            }
         }
     }
 }
