55048e3f15c53003454c0a17da8fc756365fced3
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2022-03-21 19:26:16 +0100

    themed grab game and added minigame instrs

--- Assets/CoopChaos/Scripts/Client/Game/Threat/Minigames/GrabItem.cs
+++ Assets/CoopChaos/Scripts/Client/Game/Threat/Minigames/GrabItem.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,35 @@ namespace CoopChaos
     public class GrabItem : BaseThreatMinigame
     {
         [SerializeField]
-        private Button solverButton;
+        private Button[] solverButton;
+
+        [SerializeField]
+        private Sprite[] items;
+
+        [SerializeField]
+        private string[] itemNames;
+
+        [SerializeField]
+        private TextMeshProUGUI instruction;
+
+        int correctItem = 0;
 
         public override void StartMinigame()
         {
             base.StartMinigame();
-            solverButton.onClick.AddListener(ButtonClicked);
+
+            correctItem = Random.Range(0, solverButton.Length);
+
+            instruction.text = "Nehme " + itemNames[correctItem] + " aus dem Regal.";
+
+            for (int i = 0; i < solverButton.Length; i++)
+            {
+                solverButton[i].GetComponent<Image>().sprite = items[i];
+                if(i == correctItem)
+                {
+                    solverButton[i].onClick.AddListener(() => ButtonClicked());
+                }
+            }
         }
 
         void ButtonClicked()
